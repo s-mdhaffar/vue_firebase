ac9b81646bd775d428c42ca720cb1aded03bf216
s-mdhaffar <s.mdhaffar@gmail.com>
2022-10-15 21:49:05 +0100

    Mouse Events

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
 			show   : true,
 			title  : 'Harry Potter',
 			author : 'JK Rollings',
-			age    : 45
+			age    : 45,
+			x      : 0,
+			y      : 0
 		};
 	},
 	methods : {
@@ -13,6 +15,16 @@ const app = Vue.createApp({
 		},
 		handleShow () {
 			this.show = !this.show;
+		},
+		handleEvent (e, data) {
+			console.log(e, e.type);
+			if (data) {
+				console.log(data);
+			}
+		},
+		handleMouseMove (e) {
+			this.x = e.offsetX;
+			this.y = e.offsetY;
 		}
 	}
 });
